dcbbf977fdb26f4135e2e63552ed7a18fc8f55f1
Hancy15 <sadiqmaliyu01@gmail.com>
2023-12-02 02:23:36 +0100

    a simple new python sqrt code

---
+++ sqrt_calculator.py
@@ -0,0 +1,7 @@
+import math
+
+# Get user input for the number
+number = float(input("Enter a number: "))
+
+# Calculate and print the square root
+print(f"The square root of {number} is: {math.sqrt(number)}")
